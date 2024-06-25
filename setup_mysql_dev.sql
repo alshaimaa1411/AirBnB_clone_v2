d612094b4ba85312380c6ba247629864dedfcace
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-06-25 22:31:08 +0300

    1T

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,6 @@
 -- new user
 CREATE database IF NOT EXISTS hbnb_dev_db;
-CREATE USER 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'hbnb_dev'@'localhost' WITH GRANT OPTION;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
