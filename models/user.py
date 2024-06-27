47c300a3e9fe0b00a6fadef0b79aae56755f1a99
alshaimaa1411 <ahmedameen1411@gmail.com>
2024-06-27 03:04:46 +0300

    3r

--- models/user.py
+++ models/user.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-"""This module defines a class User"""
-from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, MetaData
+"""Defines the User class."""
+from models.base_model import Base
+from models.base_model import BaseModel
+from sqlalchemy import Column
+from sqlalchemy import String
 from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class defines a user by various attributes"""
-    __tablename__ = 'users'
+    """Represents a user for a MySQL database.
+
+   views (sqlalchemy relationship): The User-Review relationship.
+    """
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
-    places = relationship("Place", cascade="all, delete", backref="user")
-    reviews = relationship("Review", cascade="all, delete", backref="user")
+    first_name = Column(String(128))
+    last_name = Column(String(128))
+    places = relationship("Place", backref="user", cascade="delete")
+    reviews = relationship("Review", backref="user", cascade="delete")
